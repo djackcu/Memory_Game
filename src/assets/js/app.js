ce495e59790ee6194c329c19b9c6c6d90e102d32
Dieter Jackson <djackcu@gmail.com>
2018-03-24 11:26:26 +0100

    adding primary source

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,10 +1,10 @@
 const memoryGame = {
-    arrayCards: [], 
-    activeCards: [],
+    arrayCards: [],
+    activeCards: 0,
     solvedCards: [],
     // Shuffle function from http://stackoverflow.com/a/2450976
     shuffleCards() {
-        var currentIndex = this.arrayCards.length,
+        let currentIndex = this.arrayCards.length,
             temporaryValue, randomIndex;
 
         while (currentIndex !== 0) {
@@ -16,10 +16,15 @@ const memoryGame = {
         }
     },
 
-    initGame(num) {
+    initGame() {
+        let num = 8;
         let newCards = [];
-        this.activeCards.splice(0, this.activeCards.length);
-        this.solvedCards.splice(0, this.solvedCards.length);
+        if (this.activeCards != 0) {
+            this.activeCards = 0;
+        }
+        if (this.solvedCards.length != 0) {
+            this.solvedCards.splice(0, this.solvedCards.length);
+        }
         for (let i = 1; i <= num; i++) {
             newCards.push(i);
             newCards.push(i);
@@ -30,15 +35,15 @@ const memoryGame = {
 
     isPair(card) {
         console.log(card);
-        if (this.activeCards.length == 0) {
-            this.activeCards.push(card);
+        if (this.activeCards == 0) {
+            this.activeCards=card;
         } else {
-            if (this.activeCards[0] == card) {
+            if (this.activeCards == card) {
                 this.solvedCards.push(card);
-                this.activeCards.pop();
+                this.activeCards=0;
                 this.arrayCards = this.arrayCards.map(x => (x == card ? 0 : x));
             } else {
-                this.activeCards.pop();
+                this.activeCards=0;
             }
         }
     },
@@ -55,29 +60,28 @@ function printAll() {
     console.log(memoryGame.solvedCards);
     console.log(memoryGame.isEndGame());
 }
-memoryGame.initGame(4);
-printAll();
-memoryGame.shuffleCards();
-printAll();
-memoryGame.isPair(1);
-printAll();
-memoryGame.isPair(1);
-printAll();
-memoryGame.isPair(4);
-printAll();
-memoryGame.isPair(2);
-printAll();
-memoryGame.isPair(2);
-printAll();
-memoryGame.isPair(2);
-printAll();
-memoryGame.isPair(3);
-printAll();
-memoryGame.isPair(3);
-printAll();
-memoryGame.isPair(4);
-printAll();
-memoryGame.isPair(4);
-printAll();
-memoryGame.initGame(4);
-printAll();
+
+/*
+ * Create a list that holds all of your cards
+ */
+
+
+/*
+ * Display the cards on the page
+ *   - shuffle the list of cards using the provided "shuffle" method below
+ *   - loop through each card and create its HTML
+ *   - add each card's HTML to the page
+ */
+
+// Shuffle function from http://stackoverflow.com/a/2450976
+
+/*
+ * set up the event listener for a card. If a card is clicked:
+ *  - display the card's symbol (put this functionality in another function that you call from this one)
+ *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
+ *  - if the list already has another card, check to see if the two cards match
+ *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
+ *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
+ *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
+ *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
+ */
