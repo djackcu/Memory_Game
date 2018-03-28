f935c91f7f1bac69ab2c69c15b428f0415896ffc
Dieter Jackson <djackcu@gmail.com>
2018-03-28 23:01:00 +0200

    feat:Update Readme

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 #Memory Game
+
+##Table of content
+*Descriptions
+*Instructions
+*Contributing
+
+###Description
+Memory Game made in vainilla javascript, html and css.
+
+###Instructions 
+The player need to pair the cards, with one scoreboard to show time and moves.The game finish when all your card are pair
+
+##### Contributing
+This repository is one of the project of Udacity's Nanodegree. Therefore, we most likely will not accept pull requests.
--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -210,6 +210,7 @@ function createDeck() {
 function restartGame() {
     document.querySelector('.modal').classList.add('hide');
     document.querySelector('.modal-back').classList.add('hide');
+    document.querySelector('.restart').classList.remove('hide');
     const panelScore = document.querySelector('.score-panel');
      const scorePanel = document.querySelectorAll('.panel');
      for(let elem of scorePanel){
@@ -222,6 +223,7 @@ function endGame() {
     clearInterval(timerId);
     document.querySelector('.modal').classList.remove('hide');
     document.querySelector('.modal-back').classList.remove('hide');
+    document.querySelector('.restart').classList.add('hide');
     const endScore = document.querySelector('.end-score');
      const scorePanel = document.querySelectorAll('.panel');
      console.log(scorePanel);
